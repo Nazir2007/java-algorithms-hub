2c125eeddef078ec612c97900d951460911558d8
Nazir2007 <nazirmammadov07@gmail.com>
2026-08-25 16:39:10 +0400

    First program 'Fibanocci' added

---
+++ src/basics/Fibanocci.java
@@ -0,0 +1,26 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Fibanocci{
+    public static void main(String[] args) {
+        System.out.println("Which Fibonacci number do you want to find?  : ");
+        Scanner in=new Scanner(System.in);
+        int nTH=in.nextInt();
+
+        int count=2,ans=0,pre=1;
+
+        if(nTH==1){
+            System.out.println("Answer is:0 previous:0");
+        }
+        else if(nTH>1){
+            while(count<=nTH){
+                int temp=ans;
+                ans+=pre;
+                pre=temp;
+                count++;
+            }
+            System.out.println("Answer is: "+ans+" previous: "+pre);
+        }
+    }
+}
